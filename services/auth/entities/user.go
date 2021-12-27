cdb55965b8bb5d7f4bd6a8c569933438dca41d07
Moritz Eich <hey@moritz.dev>
2021-12-27 17:18:37 +0100

    change: user db model

--- services/auth/entities/user.go
+++ services/auth/entities/user.go
@@ -1,13 +1,24 @@
 package entities
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	ID        string    `json:"id,omitempty" bson:"_id,omitempty"`
-	Name      string    `json:"name"         bson:"name"`
-	Email     string    `json:"email"        bson:"email"`
-	Password  string    `json:"password"     bson:"password"`
-	createdAt time.Time `                    bson:"created_at,omitempty"`
-	updatedAt time.Time `                    bson:"updated_at,omitempty"`
-	deletedAt time.Time `                    bson:"deleted_at,omitempty"`
+	ID        primitive.ObjectID `bson:"_id"`
+	Name      string             `bson:"name"`
+	Email     string             `bson:"email"`
+	Password  string             `bson:"password"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+	DeletedAt time.Time          `bson:"deleted_at"`
+}
+
+func (u *User) GetBSON() (interface{}, error) {
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
+	type my *User
+	return my(u), nil
 }
