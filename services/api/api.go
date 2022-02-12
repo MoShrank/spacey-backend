aa5764001bdeab2771a8c1166d1b3de46160d3f9
MoShrank <31897795+MoShrank@users.noreply.github.com>
2022-02-12 10:22:55 +0100

    add: prometheus metrics + endpoint in api (#19)
    Co-authored-by: Moritz Eich <hey@moritz.dev>

--- services/api/api.go
+++ services/api/api.go
@@ -6,15 +6,28 @@ import (
 	"github.com/moshrank/spacey-backend/pkg/logger"
 	"github.com/moshrank/spacey-backend/pkg/middleware"
 	"github.com/moshrank/spacey-backend/services/api/routes"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+func prometheusHandler() gin.HandlerFunc {
+	h := promhttp.Handler()
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
 func main() {
+
 	config, err := config.NewConfig()
 	log := logger.NewLogger(config)
 
 	router := gin.New()
 	router.Use(middleware.Logger(log))
 	router.Use(middleware.Recovery())
+	router.Use(middleware.PrometheusMiddleware())
+
+	router.GET("/prometheus", prometheusHandler())
 
 	routes.CreateRoutes(router, config)
 
